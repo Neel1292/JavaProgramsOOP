59d795f153dfa5a5ba296e1b7c0cd7cdb2890f72
Neel1292 <109955689+Neel1292@users.noreply.github.com>
2022-08-09 12:47:48 +0530

    Add files via upload

---
+++ SchedulExecutorService.java
@@ -0,0 +1,29 @@
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class SchedulExecutorService {
+        public static void main(String args[]) {
+        System.out.println("Creating Schedule Executor Service....");
+
+        ScheduledExecutorService execute = Executors.newScheduledThreadPool(2);
+
+        Runnable task = ()->{
+            System.out.println("Executed task at "+System.nanoTime());
+        };
+    
+        System.out.println("Submitting task at "+System.nanoTime() + " to be exectued after 5 seconds");
+        execute.schedule(task,2,TimeUnit.SECONDS);
+
+        Runnable task1 = ()->{
+            System.out.println("Executed task at "+System.nanoTime());
+        };
+       
+        System.out.println("Submitting 2nd  at "+System.nanoTime() + " to be exectued after 5 seconds");
+        execute.schedule(task1,2,TimeUnit.SECONDS);
+
+        execute.shutdown();
+        
+
+    }
+}
